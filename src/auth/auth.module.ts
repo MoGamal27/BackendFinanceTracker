c8d61fbb00933ce83a1b55a949c465a6d9272ae7
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-07-31 18:19:29 +0300

    update expiration time token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '1d' },
+        signOptions: { expiresIn: '3d' },
       }),
       inject: [ConfigService],
     }),
